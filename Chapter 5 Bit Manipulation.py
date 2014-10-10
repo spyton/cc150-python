9af89e85d0902c5154a16ab1235ca5503d85e9eb
avocadoccme <yiransh@gmail.com>
2014-10-10 01:15:48 -0700

    10-10-2014 home

---
+++ Chapter 5 Bit Manipulation.py
@@ -0,0 +1,48 @@
+# # Return bit is 1 or 0
+# def getBit(num, i):
+#     return num & (1<<i) != 0
+
+# # Set bit to 1 
+# def setBit(num, i):
+#     return num | (1<<i)
+
+# # Set bit to 0
+# def clearBit(num, i):
+#     return num & ~(1<<i)
+
+# # Set bits to 0 from most significant bit to i (inclusive)
+# def clearBitsMSBtoI(num, i):
+#     mask = (1<<i) - 1
+#     return num & mask
+
+# # Set bits to 0 from i to lease significant bit (inclusive)
+# def clearBitsIto0(num, i):
+#     mask = ~((1<<(i+1)-1)
+#     return num & mask
+
+# # Set bit to 1 or 0
+# def updateBit(num, i, v):
+#     mask = ~(1<<i)
+#     return (num&mask) | (v<<i)
+
+class Solution:
+    # 5.1
+    # 5.2
+    # 5.3
+    # 5.4
+    # 5.5
+    # 5.6
+    # 5.7
+    # 5.8
+
+if __name__ == "__main__":
+    # 5.1
+    # 5.2
+    # 5.3
+    # 5.4
+    # 5.5
+    # 5.6
+    # 5.7
+    # 5.8
+
+
