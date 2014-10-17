f0a7fa160d2b79e0a03fda77b9b3feaf85a8e374
avocadoccme <yiransh@gmail.com>
2014-10-16 23:03:13 -0700

    10-16-2014 home

--- Chapter 5 Bit Manipulation.py
+++ Chapter 5 Bit Manipulation.py
@@ -97,11 +97,52 @@ class Solution:
         return (((x&0xaaaaaaaa)>>1) | ((x&0x55555555) << 1))
     # 5.7
     def findMissing(self, A):
-        return self.findMissingRecur(A, 0)
+        return self.findMissingRecur(A)
 
-    def findMissingRecur(self, A, 0):
-
+    def findMissingRecur(self, A):
+        if sum(A) == 0: return 0
+        zeros, ones = [], []
+        for a in A:
+            if (a&1)==1:
+                a>>=1
+                ones.append(a)
+            else:
+                a>>=1
+                zeros.append(a)
+        if len(zeros) <= len(ones):
+            # LSB of x is 0
+            v = self.findMissingRecur(zeros)
+            return (v<<1)|0
+        else:
+            # LSB of x is 1
+            v = self.findMissingRecur(ones)
+            return (v<<1)|1
     # 5.8
+    def drawLine(self, screen, width, x1, x2, y):
+        start_offset = x1%8
+        first_full_byte = x1/8
+        if start_offset != 0: first_full_byte+=1
+        end_offset = x2%8
+        last_full_byte = x2/8
+        if end_offset != 7: last_full_byte-=1
+        # set full bytes
+        for i in range(first_full_byte, last_full_byte+1):
+            screen[(width/8)*y + i] = 0xff
+        # create masks for start and end of line
+        start_mask = 0xff >> start_offset
+        end_mask = (0xff >> (end_offset+1))^0xff
+        print "{:08b}".format(start_mask)
+        print "{:08b}".format(end_mask)
+        print end_offset
+        # set start and end of line
+        if (x1/8) == (x2/8):
+            mask = start_mask & end_mask
+            screen[(width/8)*y+(x1/8)] |= mask
+        else:
+            if start_offset != 0 :
+                screen[(width/8)*y+first_full_byte-1] |= start_mask
+            if end_offset != 7:
+                screen[(width/8)*y+last_full_byte+1] |= end_mask
 
 if __name__ == "__main__":
     s = Solution()
@@ -136,6 +177,11 @@ if __name__ == "__main__":
     print s.findMissing([0,1,2,3,4,5,7,8,9,10])
     # 5.8
     print "# 5.8"
-    print s.drawline([[0b00000000, 0b00000000], [0b00000000, 0b00000000]], 3, 13, 0)
-
-
+    screen = [0b00000000, 0b00000000, 0b00000000, 0b00000000]
+    s.drawLine(screen, 16, 3, 13, 0)
+    cur = 0
+    while cur < len(screen):
+        print "{:08b}".format(screen[cur]),
+        cur+=1
+        print "{:08b}".format(screen[cur])
+        cur+=1
